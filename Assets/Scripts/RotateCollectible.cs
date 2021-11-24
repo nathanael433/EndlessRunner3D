e6fc98933c4fb9d8a7f58357db5cbe358f294725
Auddrem <audreyann.martineau@gmail.com>
2021-11-24 08:41:15 -0500

    Scripts and collectible
    add rotate scripts and first collectible

---
+++ Assets/Scripts/RotateCollectible.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    [SerializeField] float speedX;
+    [SerializeField] float speedY;
+    [SerializeField] float speedZ;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
